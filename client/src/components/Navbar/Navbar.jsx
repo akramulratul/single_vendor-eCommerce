fb2d194bc07ce88e5cf16bbb7750b408fcd1da61
Akramul Ratul <akramulratul@gmail.com>
2024-01-30 14:34:54 +0600

    UI customization for e-commerce

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -18,14 +18,6 @@ const Navbar = () => {
       <div className="wrapper">
         <div className="left">
           <div className="item">
-            <img src="/img/en.png" alt="" />
-            <KeyboardArrowDownIcon />
-          </div>
-          <div className="item">
-            <span>USD</span>
-            <KeyboardArrowDownIcon />
-          </div>
-          <div className="item">
             <Link className="link" to="/products/1">
               Women
             </Link>
